ea4ac851ab768cc0794df095396fabd7f39add66
Yutong11 <1136743041@qq.com>
2020-03-27 18:20:52 +0800

    change settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,18 +74,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', # 'django.db.backends.sqlite3',
+#         'NAME': 'projectdb', # os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # 'django.db.backends.sqlite3',
-        'NAME': 'projectdb', # os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Yutong11$projectdb',
+        'USER': 'Yutong11',
+        'PASSWORD': 'He901130',
+        'HOST': 'Yutong11.mysql.pythonanywhere-services.com',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
